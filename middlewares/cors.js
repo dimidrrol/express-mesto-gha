d7c2f171093f670c177a8f953c6b22a50c4aa20d
Dmitriy Chernyakov <john_shepard_96@mail.ru>
2023-07-27 14:36:58 +0600

    PR15

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,6 +9,7 @@ const allowedCors = [
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  console.log(origin);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
